8b1a1124fc24aa2113e20b2099df9a4e8cd367c2
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2017-06-24 18:06:03 -0300

    Se agrego StocksTableModel

---
+++ src/PortfolioManager/StocksTableModel.java
@@ -0,0 +1,34 @@
+package PortfolioManager;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+public class StocksTableModel extends AbstractTableModel {
+	 
+	private List<Stock> stockList;
+	
+	public StocksTableModel(List<Stock> list) {
+		stockList = list;
+	}
+
+	@Override
+	public int getRowCount() {
+		return stockList.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 2;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Stock stock = stockList.get(rowIndex);
+		if(columnIndex == 0)
+			return (Object) stock;
+		else
+			return (Object) ("$" + stock.getValue());
+	}
+
+}
