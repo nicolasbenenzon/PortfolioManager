5d145417a560a6bbe91644c260260464523f7054
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2017-06-25 17:55:37 -0300

    Se agrego AssetTableModel que no se agrego en el commit anterior.

---
+++ src/PortfolioManager/AssetTableModel.java
@@ -0,0 +1,43 @@
+package PortfolioManager;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+public abstract class AssetTableModel<T extends Asset> extends AbstractTableModel {
+	
+	private static final long serialVersionUID = 8727850667225925336L;
+	private List<T> assetList;
+	
+	public AssetTableModel(List<T> list) {
+		assetList = list;
+	}
+
+	@Override
+	public int getRowCount() {
+		return assetList.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 2;
+	}
+	
+	@Override
+	public String getColumnName(int column) {
+		if(column == 0)
+			return "Ticker";
+		else
+			return "Price";
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		T asset = assetList.get(rowIndex);
+		if(columnIndex == 0)
+			return (Object) asset;
+		else
+			return (Object) ("$" + asset.getValue());
+	}
+
+}
